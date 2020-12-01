138a377597f50b3015c709e8b71fa5d3a1f161f1
Wil Thomason <wil.thomason@gmail.com>
2020-12-01 14:27:39 -0500

    2020, Day 1, Part 2 (and improvements to Part 1)

--- 2020/day1/solution.jl
+++ 2020/day1/solution.jl
@@ -3,7 +3,7 @@ function load_input(path)
 end
 
 function solve_1(data)
-  values :: Set{Int32} = Set()
+  values = BitSet()
   for val in data
     complement = 2020 - val
     if complement in values
@@ -14,5 +14,29 @@ function solve_1(data)
   end
 end
 
+function solve_2(data)
+  sort!(data, alg=InsertionSort)
+  len = length(data)
+  for val in data
+    l = 1
+    h = len
+    while h > l 
+      sum = val + data[l] + data[h]
+      if sum == 2020
+        return val * data[l] * data[h]
+      elseif sum > 2020
+        h -= 1
+      else
+        l += 1
+      end
+    end
+  end
+end
+
+function solve(data)
+  println(@time solve_1(data))
+  println(@time solve_2(data))
+end
+
 data = load_input("input.txt")
-println(solve_1(data))
+solve(data)
